c9756a75b07e96601ca30b13cb4127513f711003
Sujal Barnwal <sujalbarnwal9603@gmail.com>
2025-06-25 11:51:37 +0530

    User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+    fullName:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        index:true,
+        trim: true,
+        lowercase: true,
+    },
+    avatar:{
+        type: String, // Cloudinary URL
+        required: false,
+    },
+    isFavorite:{
+        type: Boolean,
+        default: false,
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken:{
+        type: String,
+        required: false,
+    },
+},
+{
+    timestamps: true,
+}
+)
+
+
+export const User= mongoose.model("User",userSchema);

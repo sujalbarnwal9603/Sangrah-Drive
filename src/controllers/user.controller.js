421f433dbfcbae683033c01307ea5e6b4d3c31b4
Sujal Barnwal <sujalbarnwal9603@gmail.com>
2025-06-27 13:30:11 +0530

    Logout and auth-middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -107,7 +107,37 @@ const loginUser=asyncHandler(async(req,res)=>{
 
 })
 
+const logoutUser=asyncHandler(async(req,res)=>{
+    // req.user._id   //it's bcz aut middleware
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+
+    return res
+        .status(200)
+        .clearCookie("accessToken",options)
+        .clearCookie("refreshToken",options)
+        .json(new ApiResponse(200,{},"Logout Successfully"))
+})
+
 
 export {
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
 }

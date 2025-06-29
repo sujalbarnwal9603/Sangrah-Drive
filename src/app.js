ebaad06a215dd0dd1f9dfab76401fb24cb2f3acf
Sujal Barnwal <sujalbarnwal9603@gmail.com>
2025-06-29 11:36:15 +0530

    300mb chnages for updation

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors({
     credentials:true
 }))
 
-app.use(express.json({limit:"16k"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.json({limit:"350mb"}));
+app.use(express.urlencoded({extended:true,limit:"350mb"}));
 
 app.use(express.static("public"));
 app.use(cookieParser());
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import path from "path";
 
 const storage=multer.diskStorage({
     destination:function (req,file,cb){
@@ -9,7 +10,9 @@ const storage=multer.diskStorage({
     }
 })
 
-export const upload =multer({
-    storage,
-    
-})
+const upload = multer({
+  storage,
+  limits: { fileSize: 300 * 1024 * 1024 }, // Optional: 300MB max per file
+});
+
+export const uploadFiles = upload.fields([{ name: "file", maxCount: 10 }]);
